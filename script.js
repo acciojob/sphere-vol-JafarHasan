2da865c0b353ca630804453cc8093b7d7739ece3
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-06-17 19:53:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault(); // Prevent form submission
+    var radius = parseFloat(document.getElementById('radius').value);
+
+    if (!isNaN(radius)) {
+        var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        document.getElementById('volume').value;// = volume.toFixed(2); // Display volume with two decimal places
+    } 
+	
   
 } 
 
